04c99cd0af084e31b7e80b32f767c30d710a46a2
unknown <hjalmar_thulstrup@hotmail.com>
2019-04-07 18:45:53 +0200

    the outcome is subpar. needs a lot of tweaking

--- maker.py
+++ maker.py
@@ -3,17 +3,14 @@ import argparse, os
 
 def import_clips(dir_path):
     clips = []
-    for (path, dirs, files) in os.walk(dir_path):
-        for file in files:
-            filename = os.path.join(path, file)
-            clip = VideoFileClip(filename)
-            if filename[:-4] == ".webm":
-                clips.append(clip)
+    for filename in os.listdir(dir_path):
+        if filename.endswith('.webm'):
+            clips.append(VideoFileClip(dir_path + filename))
     return clips
 
 
 def make_compilation(clips, name):
-    final_clip = concatenate_videoclips(clips)
+    final_clip = concatenate_videoclips(clips, method='compose')
     try:
         final_clip.write_videofile(name)
         print("Compilation saved as " + name)
@@ -27,4 +24,9 @@ if __name__ == '__main__':
     parser.add_argument(
         '-n', '--name', help="The name and file extension you want to give the compilation video.")
     args = parser.parse_args()
-    make_compilation(import_clips(args.directory), args.name)
+    _dir = args.directory
+    name = args.name
+    if _dir != None:
+        if _dir[-1:] != "/" or _dir[-1:] != "\\":
+            _dir = _dir + "/"
+    make_compilation(import_clips(_dir), name)
